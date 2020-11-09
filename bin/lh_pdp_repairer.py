c3708ffa970f2800bff83768bff986e691ed43ed
Ocean <gzhehai@foxmail.com>
2020-11-09 17:19:59 +0800

    Fix incorrect words

--- bin/lh_pdp_repairer.py
+++ bin/lh_pdp_repairer.py
@@ -11,7 +11,7 @@ import sys
 1. 在项目根目录执行,比如: python bin/lh_pdp_repairer.py
 2. 在项目 bin 目录执行,比如: python lh_pdp_repairer.py
 
-为避免在任意目录下执行 bin/ 目录下的 .py 文件都可以正常 working (解决 Not found Named xxx Module 问题)
+为解决在任意目录下执行 bin/ 目录下的 .py 文件都可以正常 working (Not found Named xxx Module 问题)
 我们把项目根目录作为 Python 解释器查找 Module 的一部分设置到 path 上
 """
 sys.path.append(os.path.join(os.path.abspath(os.path.dirname(os.path.abspath(__file__)) + os.path.sep + "..")))
